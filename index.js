35456af95d465ab97fdad4ce0f0cbeba3478cdff
Gwachukwu <gwachukwuelebeke@gmail.com>
2020-02-12 22:27:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+document.querySelector(".calculate").addEventListener("click", calculate);
+
+document.querySelector(".clear").addEventListener("click", clear);
+
+function calculate() {
+  let price = document.getElementById("price").value;
+  let percentage = document.getElementById("percentage").value;
+  let numberOfCustomers = document.getElementById("customers").value;
+  let tip = (percentage / 100) * price;
+  let customerTab = tip / numberOfCustomers;
+
+  if (price === "") {
+    document.querySelector(
+      ".total-tips"
+    ).innerHTML = `Please enter total price`;
+  } else if (numberOfCustomers === "") {
+    document.querySelector(
+      ".unit-cost"
+    ).innerHTML = `Please enter number of customers`;
+  } else {
+    document.querySelector(
+      ".total-tips"
+    ).innerHTML = `Total tip for this service is &#x20A6;${tip}`;
+    document.querySelector(
+      ".unit-cost"
+    ).innerHTML = `Each customer will pay &#x20A6;${customerTab}`;
+  }
+};
+
+function clear() {
+  document.getElementById("price").value = "";
+  document.getElementById("percentage").value = "0";
+  document.getElementById("customers").value = "";
+  document.querySelector(".total-tips").innerHTML = "";
+  document.querySelector(".unit-cost").innerHTML = "";
+};
